77a0acb7e04dd16366e19e7a70ab17bafe787632
SandipPandhare <93873281+SandipPandhare@users.noreply.github.com>
2023-05-24 20:34:36 +0530

    Add files via upload

---
+++ program5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class program5 
+{
+    public static void main(String args[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the string : ");
+        String str=sobj.nextLine();
+        
+    }    
+}
